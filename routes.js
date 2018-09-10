8634af8888f49135919dda2eb2d6d89b70a2a1cc
romulo5 <romulofloresta@gmail.com>
2018-09-10 10:31:29 -0300

    fix 403 response

--- routes.js
+++ routes.js
@@ -2,25 +2,25 @@ const request = require("request");
 const appRouter = app => {
   app.get("/busca", function(req, res) {
     const cpf = req.param("cpf");
-    const cnpj = req.param("cnpj") 
+    const cnpj = req.param("cnpj");
     const service_url =
       "https://apicpe.sebrae.com.br/v1/pessoasjuridicas/cpf-responsavel/";
- 
-   request.get(service_url + cpf, (error, response, body) => {
+
+    request.get(service_url + cpf, (error, response, body) => {
       console.log("error:", error); // Print the error if one occurred
-      console.log("statusCode:", response && response.statusCode); 
-      
+      console.log("statusCode:", response && response.statusCode);
+
       const data = JSON.parse(body);
       const cnpj_from_response = data[0].cnpj;
-      if (cnpj == cnpj_from_response) {
-        res.status(200).send({match:true});
-
+      if (response.statusCode === 403) {
+        res.status(200).send(body);
+      } else if (cnpj == cnpj_from_response) {
+        res.status(200).send({ match: true });
       } else {
-        res.status(200).send({match:false});
+        res.status(200).send({ match: false });
       }
-
     });
   });
-}
+};
 
 module.exports = appRouter;
