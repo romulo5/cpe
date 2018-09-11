858a1b1d8a36b295210a72e4044bbeea77b3ffa6
romulo5 <romulofloresta@gmail.com>
2018-09-11 17:16:48 -0300

    bugfix

--- routes.js
+++ routes.js
@@ -10,16 +10,16 @@ const appRouter = app => {
     request.get(service_url + cpf, (error, response, body) => {
       console.log("error:", error); // Print the error if one occurred
       console.log("statusCode:", response && response.statusCode);
-      const data = JSON.parse(body);
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       if (response.statusCode === 403) {
         res.status(200).send({match: false});
-    } else if (response.statusCode === 200 && !isEmpty(data)) {
+    } else if (response.statusCode === 200 && !isEmpty(body)) {
+      const data = JSON.parse(body);
       const cnpj_from_response = data[0].cnpj;   
       console.log(cnpj_from_response)
       res.status(200).send({ match: cnpj === cnpj_from_response});
-      } else {
+      } else {  
         res.status(200).send({match: false})
       }
     });
